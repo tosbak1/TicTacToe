ccc13c2871aceac9cf4e857f21a9a1fabd6a5a5a
toyinodujebe <toyinodujebe@cmail.carleton.ca>
2016-10-25 14:41:15 -0400

    Starting working on the view

--- src/TTTView.java
+++ src/TTTView.java
@@ -1,11 +1,10 @@
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 import java.util.Observable;
 import java.util.Observer;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JTextArea;
+import javax.swing.*;
 
 public class TTTView extends JFrame implements Observer {
 
@@ -14,18 +13,32 @@ public class TTTView extends JFrame implements Observer {
 	private JFrame frame;
 	private JTextArea area = new JTextArea();
 	private TTTModel md;
+	private JPanel jp;
 	
-	public TTTView(TTTModel md, ActionListener listener){
+	public TTTView(TTTModel md, ActionListener al){
 		this.md = md;
 		this.listener = listener;
+		jp = new JPanel();
+		
 		frame = new JFrame("TicTacToe");
 		frame.setSize(300, 300);
 		
 		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	    frame.setLayout(new BorderLayout());
 	    
+	    jp.setLayout(new GridLayout(3,3));
+	    buttons = new JButton[md.getSize()][md.getSize()];
 	    
-		
+	    for(int i = 0; i< 3; i++){
+	    	for(int j = 0; j<3; j++){
+	    		buttons[i][j] = new JButton("");
+	    		jp.add(buttons[i][j]);
+	    		buttons[i][j].addActionListener(listener);
+	    	}
+	    }
+	    
+	    frame.getContentPane().add(jp, BorderLayout.CENTER);
+	    frame.setVisible(true);
 	}
 	
 	
@@ -33,6 +46,12 @@ public class TTTView extends JFrame implements Observer {
 	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public static void main(String[] args){
+		TTTModel tm = new TTTModel(3);
+		//ActionListener al = new ActionListener();
+		TTTView tits = new TTTView(tm);
 	}
 
 }

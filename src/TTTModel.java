f47f867f2a03257ab7b0544e49c5033e2de02670
CoachTo <tydcool@gmail.com>
2016-10-29 19:33:48 -0400

    Fixed all the checks and refactored accordingly

--- src/TTTModel.java
+++ src/TTTModel.java
@@ -17,7 +17,21 @@ public class TTTModel extends Observable {
 		this.Board = new String[size][size];
 		turn = true;		//player turn is set to true;
 		canPlay = true;
+		this.initBoard();
 
+	}
+	
+	
+	//Initialize the board
+	public void initBoard(){
+		for(int i=0;i<size;i++){
+			for (int j=0;j<size;j++ ){
+				Board[i][j]="*";
+				//this.setChanged();
+				//this.notifyObservers();
+				
+			}
+		}
 	}
 	
 	//This inverts the plater turn to false
@@ -33,16 +47,16 @@ public class TTTModel extends Observable {
 	//a user can play if the value at that block is null 
 	public void play(int x, int y){
 		if(canPlay){
-			if(Board[x][y] == null){
+			if(Board[x][y].equals("*")){
 				if(turn){
 					player = "Player1";
 					Board[x][y] = "X";
-					checker(x,y);
+					checker();
 				}
 				else{
 					player = "Player2";
 					Board[x][y] = "O";
-					checker(x,y);
+					checker();
 				}
 			}
 		}
@@ -55,8 +69,8 @@ public class TTTModel extends Observable {
 
 	}
 
-	private void checker(int x, int y) {
-		if(this.playerWon(x,y)){
+	private void checker() {
+		if(this.playerWon()){
 			canPlay = false;
 			this.setChanged();
 			this.notifyObservers();
@@ -85,6 +99,7 @@ public class TTTModel extends Observable {
 		this.Board = new String[size][size];
 		this.canPlay = true;
 		turn = true;		//player turn is set to true;
+		this.initBoard();
 		this.setChanged();
 		this.notifyObservers();
 		
@@ -101,37 +116,91 @@ public class TTTModel extends Observable {
 		return true;
 	}
 	*/
-	public boolean wonHor(int x){
-		for(int i = 0; i<this.size-1; i++){
-			if((Board[x][i]!=(Board[x][i+1])) && Board[x][i] != null) return false;
-
+	public boolean wonHor(){
+		int count1 =0; 
+		int count2 =0;
+		for(int i=0;i<size;i++){
+			for (int j=0;j<size-1;j++ ){
+				if((Board[i][j].equals(Board[i][j+1])) && !Board[i][j].equals("*")){
+					if(Board[i][j].equals("X")){
+						count1++;
+					}else if(Board[i][j].equals("O")){
+						count2++;
+					}
+					
+				}
+			}
+			
 		}
-		return true;
+
+		if(count1 ==size-1||count2==size-1){
+			
+			return true;
+		}
+		
+		
+		return false;
 	}
 	//checks the vertical blocks if a user has won
-	public boolean wonVet(int y){
-		for(int i = 0; i<this.size-1; i++){
-			if((Board[i][y]!=(Board[i+1][y])) && Board[i][y] != null) return false;
-
+	public boolean wonVet(){
+		int count1=0;
+		int count2=0;
+		for(int i=0;i<size-1;i++){
+			for (int j=0;j<size;j++ ){
+				if((Board[i][j].equals(Board[i+1][j])) && !Board[i][j].equals("*")){
+					if(Board[i][j].equals("X")){
+						count1++;
+					}else if(Board[i][j].equals("O")){
+						count2++;
+					}
+				}
+					
+			}
+			
 		}
-		return true;
+		
+		if(count1 ==size-1 || count2==size-1){
+			return true;
+		}
+			return false;
+	
 	}
+	
 	
 	//check diagonal
 	public boolean wonDiag(){
-		for(int i = 0; i< size-1; i++){
-			if((Board[i][i]!=(Board[i+1][i+1]))) return false;	//Diagonal 
-		}	
-		for(int i = 0, j = size-1; i < size -1 && j >= 0; i++, j--){
-			if((Board[i][j]!=(Board[i+1][j-1]))) return false;
+		int count1 =0; 
+		int count2 =0;
+		for(int i=0; i<size-1;i++){
+			if ( Board[i][i].equals(Board[i+1][i+1]) &&  !Board[i].equals("*"))
+				if(Board[i][i].equals("X")){
+					count1++;
+				}else if(Board[i][i].equals("O")){
+					count2++;
+				}
+			
 		}
-		return true;
+	
+		for(int i= 0, j=size-1; i< size-1 && j>=1 ; i++, j--){
+			if( Board[i][j].equals(Board[i+1][j-1]) && !Board[i][j].equals("*"))
+				if(Board[i][j].equals("X")){
+					count1++;
+				}else if(Board[i][j].equals("O")){
+					count2++;
+				}
+		}
+
+		if(count1 ==size-1 || count2==size-1)
+			return true;
+			
+			return false;
 	} 
 	
 	//check if a player has won based on a move
-	public boolean playerWon(int x, int y){
-		if(wonVet(y) || wonDiag() || wonHor(x)){ 
-			return true;} 
+	public boolean playerWon(){
+		if(wonVet() || wonDiag() || wonHor()){ 
+			return true;
+		} 
 		return false;
 	}
 	
